e624c6db97de74f8574fb82e770be16e7c718eba
M Hatton <53306899+miahatton@users.noreply.github.com>
2021-01-30 07:43:36 +0000

    Improved comments in Light class

--- src/cis5027/project/clients/lightapp/components/Light.java
+++ src/cis5027/project/clients/lightapp/components/Light.java
@@ -8,6 +8,10 @@ import java.awt.geom.Point2D;
 
 import cis5027.project.clients.helpers.Appliance;
 
+/**
+ * @author miahatton
+ * Light class draws an array of lights whose colour can be adjusted
+ */
 
 public class Light extends Appliance {
 	
@@ -35,6 +39,77 @@ public class Light extends Appliance {
 	private int 	circleSize;
 	private int		radius;
 	
+	/*
+	 * Constructor
+	 * 
+	 * @param width
+	 * @param height
+	 * @param num
+	 */
+	public Light(int width, int height, int num) {
+
+		if(num > 0) {
+				
+			lightColor = MAX_COLOR; 
+			panelWidth = width;
+			panelHeight = height;
+			numLights = num;
+				
+			widthPad = Math.floorDiv(panelWidth, 20);	// 5% of total width
+			heightPad = Math.floorDiv(panelHeight, 20); // 5% of total width
+			gap = Math.floorDiv(panelWidth, 100); // 1% of total width
+				
+			circleSize = Math.floorDiv(panelWidth - 2 * widthPad - (numLights - 1) * gap, numLights);
+			radius = (int) circleSize / 2;
+				
+		}
+			
+			
+	}
+	
+	/*
+	 * Draws the lights
+	 */
+	public void draw(Graphics g) {
+
+		Graphics2D g2d = (Graphics2D) g;
+			
+		// Make the centre always 10% brighter than outside, to make the lights look a bit 3D
+		int edgeColorVal = (int) Math.max(getCurrentBrightness()  * 0.9, 0);
+			
+		Color[] colors = {lightColor, new Color(edgeColorVal, edgeColorVal, (int) edgeColorVal/3)}; // introduce a bit of yellow
+		float[] dist = {0.0f, 0.8f}; 
+				
+		int startX;	
+		int startY;	
+			
+		/*
+		 * Loop through the number of lights (rows and columns) and draw them.
+		 */
+		for (int j = 0; j < numLights; j++) {
+				
+			startY = heightPad + j * (gap + circleSize);
+				
+			for (int i = 0; i < numLights; i++) {
+					
+				startX = widthPad + i * (gap + circleSize);
+					
+				Point2D center = new Point2D.Float(startX + radius, startY + radius);
+				RadialGradientPaint gradient = new RadialGradientPaint(center, radius, dist, colors);
+				g2d.setPaint(gradient);
+				// add colour
+				g2d.fillOval(startX, startY, circleSize, circleSize);
+			}
+
+		}
+		
+	}
+	
+	
+	/*
+	 * Getters and Setters
+	 */
+	
 	public void setLightColor (int brightnessValue) throws IllegalArgumentException {
 		
 		int newBrightness = (int) brightnessValue * 255 / 100;
@@ -43,69 +118,12 @@ public class Light extends Appliance {
 		panel.repaint();
 		
 	}
-	
-	// constructor
-	public Light(int width, int height, int num) {
 
-		if(num > 0) {
-			
-			lightColor = MAX_COLOR; 
-			panelWidth = width;
-			panelHeight = height;
-			numLights = num;
-			
-			widthPad = Math.floorDiv(panelWidth, 20);	// 5% of total width
-			heightPad = Math.floorDiv(panelHeight, 20); // 5% of total width
-			gap = Math.floorDiv(panelWidth, 100); // 1% of total width
-			
-			circleSize = Math.floorDiv(panelWidth - 2 * widthPad - (numLights - 1) * gap, numLights);
-			radius = (int) circleSize / 2;
-			
-		}
-		
-		
-	}
 	
 	public int getCurrentBrightness() {
 		return lightColor.getRed();  // red and green always equal and maximum values in Color() call
 	}
-	
-	// draw method
-	public void draw(Graphics g) {
 
-		Graphics2D g2d = (Graphics2D) g;
-		
-		// Make the centre always 10% brighter than outside
-		int edgeColorVal = (int) Math.max(getCurrentBrightness()  * 0.9, 0);
-		
-		Color[] colors = {lightColor, new Color(edgeColorVal, edgeColorVal, (int) edgeColorVal/3)}; // introduce a bit of yellow
-		float[] dist = {0.0f, 0.8f}; 
-		
-		//g.setColor(lightColor);
-			
-		int startX;	
-		int startY;	
-		
-		for (int j = 0; j < numLights; j++) {
-			
-			startY = heightPad + j * (gap + circleSize);
-			
-			for (int i = 0; i < numLights; i++) {
-				
-				startX = widthPad + i * (gap + circleSize);
-				
-				Point2D center = new Point2D.Float(startX + radius, startY + radius);
-				RadialGradientPaint gradient = new RadialGradientPaint(center, radius, dist, colors);
-				g2d.setPaint(gradient);
-	
-				g2d.fillOval(startX, startY, circleSize, circleSize);
-			}
-
-		}
-		
-		
-	}
-	
 	public int getIncrementer() {
 		return incrementer;
 	}
