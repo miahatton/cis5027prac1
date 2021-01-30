7c8faea4d77537b5cab7653e4b5f9c49ec1ce126
M Hatton <53306899+miahatton@users.noreply.github.com>
2021-01-30 07:47:08 +0000

    Improved comments in ValueButtonPanel class

--- src/cis5027/project/clients/helpers/ValueButtonPanel.java
+++ src/cis5027/project/clients/helpers/ValueButtonPanel.java
@@ -8,12 +8,22 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+/**
+ * @author miahatton
+ * Used in all of the GUIs of the program, this is a superclass that adds a label, text input field and button to a panel.
+ */
 abstract public class ValueButtonPanel extends JPanel implements ActionListener {
 	
 	protected JLabel 		label;
 	protected JTextField 	textField;
 	protected JButton 	button;
 
+	/**
+	 * Constructor
+	 * @param labelText		Helper text to appear next to input field
+	 * @param defaultVal	Default text in input field
+	 * @param btnText		Button text
+	 */
 	public ValueButtonPanel (String labelText, String defaultVal, String btnText) {
 		this.label 		= new JLabel(labelText);
 		this.textField 	= new JTextField(defaultVal, 5);
