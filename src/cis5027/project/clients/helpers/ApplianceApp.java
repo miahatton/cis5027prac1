2c7c126f8d937c3f8be3a89d8aa54320dc3781af
M Hatton <53306899+miahatton@users.noreply.github.com>
2021-01-30 06:42:06 +0000

    Improved comments in ApplianceApp class

--- src/cis5027/project/clients/helpers/ApplianceApp.java
+++ src/cis5027/project/clients/helpers/ApplianceApp.java
@@ -7,15 +7,22 @@ import javax.swing.JOptionPane;
 
 import cis5027.project.clients.ClientConnectPanel;
 
+/*
+ * Abstract class extended by the LightApp and FanApp classes
+ * The class implements a GUI to control the light and fan clients and display the light/fan objects.
+ */
 public abstract class ApplianceApp extends JFrame {
 
-	public 		AbstractClient 				client;
+	public 		AbstractClient 		client;
 	protected 	ClientConnectPanel 	cPanel;
 	protected 	int 				port;
 	protected 	String 				ip;
 	
 	protected 	String 				clientType;
 	
+	/*
+	 * Calls the draw method to set up GUI and adds a ClientConnectPanel instance to connect to the server.
+	 */
 	public void go() {
 		draw();
 		
@@ -29,6 +36,10 @@ public abstract class ApplianceApp extends JFrame {
 	
 	abstract protected void draw();
 	
+	/*
+	 * Allows messages to be displayed to the user
+	 * @param msg
+	 */
 	public void displayMessage(String msg) {
 		cPanel.displayMessage(msg);
 	}
